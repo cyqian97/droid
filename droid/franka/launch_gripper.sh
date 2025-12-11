13ea9136b5a8bfc39aab33dcaf01c5bc02eafa8c
Chengyuan Qian <cyqian97@hotmail.com>
2025-12-11 17:28:53 -0500

    fixes for nuc

--- droid/franka/launch_gripper.sh
+++ droid/franka/launch_gripper.sh
@@ -4,8 +4,9 @@ find /root/miniconda3 -type d -name "lib" | sudo tee /etc/ld.so.conf.d/conda-pol
 # Update cache
 sudo ldconfig
 
-source ~/miniconda3/etc/profile.d/conda.sh
+source /root/miniconda3/etc/profile.d/conda.sh
 conda activate polymetis-local
 pkill -9 gripper
-chmod a+rw /dev/ttyUSB0
-launch_gripper.py gripper=robotiq_2f gripper.comport=/dev/ttyUSB0
+launch_gripper.py gripper=franka_hand
+# chmod a+rw /dev/ttyUSB0
+# launch_gripper.py gripper=robotiq_2f gripper.comport=/dev/ttyUSB0
