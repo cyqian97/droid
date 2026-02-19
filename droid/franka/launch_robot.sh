5f83b47dc66ebe71146c86b682cdd3316c4a6d29
Huang Yanjia <sam.louis0707@gmail.com>
2026-02-18 19:39:05 -0600

    change log output

--- droid/franka/launch_robot.sh
+++ droid/franka/launch_robot.sh
@@ -8,4 +8,4 @@ source /root/miniconda3/etc/profile.d/conda.sh
 conda activate polymetis-local
 pkill -9 run_server
 pkill -9 franka_panda_cl
-launch_robot.py robot_client=franka_hardware
+launch_robot.py robot_client=franka_hardware 2>&1 | tee /tmp/franka_robot.log
